f4563ac6a15e81b001e95cc4cfbb2171d9102c15
Fefaodessa <fefaodessa@gmail.com>
2022-03-22 12:14:58 +0200

    DZ 7 TEST 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,26 +7,35 @@ const allGalleryEl = document.querySelector('.gallery');
 
 const galleryEl = galleryItems.map(galleryItem => {
 
-return  `<li class="gallery__item">
-            <a class="gallery__item" href="${galleryItem.original}">
+return      `<a class="gallery__item" href="${galleryItem.original}">
                 <img class="gallery__image" 
                     src="${galleryItem.preview}" 
-                    alt="${galleryItem.description}" />
-            </a>
-        </li>`
+                    alt="" title="${galleryItem.description}" />
+            </a>`
 }).join('');
 
 allGalleryEl.insertAdjacentHTML("afterbegin", galleryEl);
 
+const lightbox = new SimpleLightbox('.gallery a', {captionDelay : 250});
+
+
 allGalleryEl.addEventListener('click', onOriginalPicture);
 
+
 function onOriginalPicture(event){
-  
+
     if(event.target.nodeName !== "IMG"){
         return 
-      }
-  
-      event.preventDefault()
-
-
     }
+    event.preventDefault()
+    lightbox.open();
+    window.addEventListener("keydown", offModal);
+
+}
+
+function offModal(event){
+    if(event.code !== "Escape"){
+    return
+    }
+    lightbox.close()
+  }
